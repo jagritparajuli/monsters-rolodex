78343f43121db42b2f43b53a79db65eb01d6d150
jagritparajuli <jagritparajuli99@gmail.com>
2020-07-30 18:06:41 +0545

    Updates

---
+++ precache-manifest.5a422c9d6342ef8c7fc2af4abc8050bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e63be98f9387ec1ee4cf33bde1233a8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "42888df40e311aae763f",
+    "url": "/monsters-rolodex/static/css/main.98255094.chunk.css"
+  },
+  {
+    "revision": "6952c745d55c5a0fa7de",
+    "url": "/monsters-rolodex/static/js/2.e47ca8d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e47ca8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42888df40e311aae763f",
+    "url": "/monsters-rolodex/static/js/main.d3abc717.chunk.js"
+  },
+  {
+    "revision": "676fa6fe07c881f9e750",
+    "url": "/monsters-rolodex/static/js/runtime-main.9c224c16.js"
+  }
+]);
